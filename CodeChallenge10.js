1ca7a032de4799149a8c18aa573876af453a02c2
Wasilat Braimah <braimahwasilat@gmail.com>
2022-10-03 03:26:02 +0100

    update files

--- CodeChallenge10.js
+++
@@ -1,8 +0,0 @@
-//Create a function that filters out negative numbers
-
-function filterNegativeNumbers(array) {
-    return array.filter(function (num){
-    return num > 0
-    })
-}
-console.log(filterNegativeNumbers([-4,-5,-8,10,45,66,-90,-45,35]))
