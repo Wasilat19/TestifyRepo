d6bb2da5aaecc1873c3ddd471fb46cc679227a3a
Wasilat Braimah <braimahwasilat@gmail.com>
2022-10-25 22:45:13 +0100

    add unit test(task26)

--- unit_testing/bodmas.py
+++
@@ -1,7 +0,0 @@
-
-def addition(x1, x2):
-    return x1 + x2
-
-
-def subtraction(x1, x2):
-    return x1 - x2
---
+++ unit_testing/compare.py
@@ -0,0 +1,13 @@
+
+def compare_strings(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+
+
+def compare_numbers(a, b):
+    if a == b:
+        return True
+    else:
+        return False
